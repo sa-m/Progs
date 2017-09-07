47ef04d644489f42fa3c82dc09ea0a8c829a3b73
Sa_M <sa99xm@gmail.com>
2017-09-07 19:42:54 +0530

    Updated Programs till 01-09-2017

--- JAVA/1-09-17/Exec.java
+++ JAVA/1-09-17/Exec.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 abstract class Employee
 {
 	String name,p_name;
@@ -9,16 +9,19 @@ class SWengg extends Employee
 {
 	Scanner sc=new Scanner(System.in);
 	public void work()
-	{
-		Employee e = new SWengg();
+	{	Employee e = new SWengg();
 		System.out.print("Enter Name : ");
 		e.name = sc.nextLine();
 		System.out.print("Enter ID : ");
 		e.id = sc.nextInt();
-		System.out.print("Enter Project Name : ") 
+		//System.out.print("Enter Project Name : "); 		//Scanner Cannot take 2 strings in the Same Program
 		e.p_name = "Interface";
+
+		// display Work, name , ID
+		System.out.println("\n\nProject Name : "+e.p_name);
+		System.out.println("Name : "+e.name);
+		System.out.println("Id : "+e.id);
 	}
-	public void disp()
 }
 interface Manager
 {
@@ -31,8 +34,10 @@ public class Exec implements Manager
 	public static void main(String args[])
 	{
 		Employee e = new SWengg();
+		e.work();
 		Manager m = new Exec();
-		System.out.println("Project Name : "+e.p_name);
+		//Display  Manager Name, Manager ID
+		//System.out.println("\nProject Name : "+e.p_name);
 		System.out.println("Manager Name : "+m.M_name);
 		System.out.println("Manager Id : "+m.M_id);
 	}
